4cbf16c3f61af625de367df3b857f0dc605dfe48
plasma_node <19627847+plasma-node@users.noreply.github.com>
2024-10-22 06:06:25 -0700

    Update ecr.d.ts

--- src/ecr.d.ts
+++ src/ecr.d.ts
@@ -295,7 +295,7 @@ export interface Registry {
     	* Added from Cent's unpublished TS edits
  	*/
 
-	find<T extends unknown[]>(this: Registry, ctype: T, value: T): entity?;
+	find<T extends unknown[]>(this: Registry, ctype: T, value: T): Entity?;
 
 	/**
 	 * Creates a [view](https://centau.github.io/ecr/api/View.html) for all entities with the specified components.
